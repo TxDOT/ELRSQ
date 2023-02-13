476ff5fd676daaaca5f5616a7f831a55892342b5
Eric Schall <eschal-c@txdot.gov>
2023-02-12 21:03:19 -0600

    wizard logic

--- script/wizard.js
+++ script/wizard.js
@@ -2,10 +2,16 @@ function noControlSection() {
     $("#rowInputControlSection").hide();
     $("#rowInputMilepointMeasure").hide();
     $("#rowInputRouteName").show();
-    $("#rowInputDistanceFromOrigin").show();
+
+    if (!($("#inputRouteName").prop("disabled"))){
+        $("#rowInputDistanceFromOrigin").show();
+    }
+    $("#inputRouteName").prop("disabled", false);
+    
 }
 
 function noMilepointMeasure() {
+    $("#inputControlSection").prop("disabled", true);
     $("#rowInputMilepointMeasure").hide();
     $("#rowInputRouteName").show();
     $("#rowInputDistanceFromOrigin").show();
@@ -20,19 +26,38 @@ function noRouteName() {
     $("#rowInputRouteName").hide();
     $("#rowInputDistanceFromOrigin").hide();
     $("#rowInputReferenceMarker").hide();
+
     $("#rowInputCoordinates").show();
+    $("#inputControlSection").prop("disabled", false);
 }
 
 function noReferenceMarker() {
+    console.log("noReferenceMarker");
     $("#rowInputReferenceMarker").hide();
     $("#rowInputCoordinates").show();
+
+    if ($("#inputControlSection").prop("disabled")){
+         $("#inputRouteName").prop("disabled", true);
+    }
+
+    if ($("#inputControlSection").prop("disabled")){
+        $("#rowInputMatchCoordinates").show();
+    }
+
+    /*if (!($("#rowInputControlSection").prop("hidden"))) {
+        $("#rowInputMatchCoordinates").show();
+    }*/
+
 }
 
 function restartWizard() {
+    $("#rowInputMatchCoordinates").hide();
     $("#rowInputCoordinates").hide();
+    $("#inputRouteName").prop("disabled", false);
     $("#rowInputRouteName").hide();
     $("#rowInputDistanceFromOrigin").hide();
     $("#rowInputReferenceMarker").hide();
+    $("#inputControlSection").prop("disabled", false);
     $("#rowInputControlSection").show();
     $("#rowInputMilepointMeasure").show();
 }
