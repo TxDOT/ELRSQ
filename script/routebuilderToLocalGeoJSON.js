b09ada33ff7ee096d587f81c2c62aaa2b4651e8b
Eric Schall <eschal-c@txdot.gov>
2023-02-20 11:45:37 -0600

    Update routebuilderToLocalGeoJSON.js

--- script/routebuilderToLocalGeoJSON.js
+++ script/routebuilderToLocalGeoJSON.js
@@ -3,39 +3,42 @@
 //Draws user defined project on the map 
 function parseGeometryToGeoJSON(prjGeom) {
   console.log("parseGeometryToGeoJSON");
-  for (var a = 0; a < prjGeom.length; a++) {
-    console.log("print geom level A: ");
-    console.log(prjGeom[a]);
-    console.log("Feature: " + a);
+  for (var nFeatureCollection = 0; nFeatureCollection < prjGeom.length; nFeatureCollection++) {
+    console.log("print geom level FeatureCollection: ");
+    console.log(prjGeom[nFeatureCollection]);
+    console.log("Feature: " + nFeatureCollection);
 
-    for (var b = 0; b < prjGeom[a].length; b++) {
+    for (var nFeature = 0; nFeature < prjGeom[nFeatureCollection].length; nFeature++) {
       let coordinates = '';
 
-      for (var c = 1; c < prjGeom[a][b].length; c++) {
-        console.log("print geom level C: ");
-        console.log(prjGeom[a][b][c]);
-        console.log("RTE_NM: " + prjGeom[a][b][c][0]);
-        console.log("BDFO: " + prjGeom[a][b][c][1]);
-        console.log("EDFO: " + prjGeom[a][b][c][2]);
-        console.log("Color: " + prjGeom[a][b][c][3]);
-        console.log("Width: " + prjGeom[a][b][c][4]);
-        console.log("Desc: " + prjGeom[a][b][c][5]);
+      for (var nLineString = 1; nLineString < prjGeom[nFeatureCollection][nFeature].length; nLineString++) {
+        console.log("print geom level LineString: ");
+        console.log(prjGeom[nFeatureCollection][nFeature][nLineString]);
+        console.log("RTE_NM: " + prjGeom[nFeatureCollection][nFeature][nLineString][0]);
+        console.log("BDFO: " + prjGeom[nFeatureCollection][nFeature][nLineString][1]);
+        console.log("EDFO: " + prjGeom[nFeatureCollection][nFeature][nLineString][2]);
+        console.log("Color: " + prjGeom[nFeatureCollection][nFeature][nLineString][3]);
+        console.log("Width: " + prjGeom[nFeatureCollection][nFeature][nLineString][4]);
+        console.log("Desc: " + prjGeom[nFeatureCollection][nFeature][nLineString][5]);
 
 
 
-        propertiesText = `"properties": { "RTE_NM": "` + prjGeom[a][b][c][0] + `", "BDFO": ` + prjGeom[a][b][c][1] + `, "EDFO": ` + prjGeom[a][b][c][2] + `,"Color": "` + prjGeom[a][b][c][3] + `", "Width": ` + prjGeom[a][b][c][4] + `,"Desc": "` + prjGeom[a][b][c][5] + `"}`
+        propertiesText = `"properties": { "RTE_NM": "` + prjGeom[nFeatureCollection][nFeature][nLineString][0] +
+          `", "BDFO": ` + prjGeom[nFeatureCollection][nFeature][nLineString][1] + `, "EDFO": ` + prjGeom[nFeatureCollection][nFeature][nLineString][2] +
+          `,"Color": "` + prjGeom[nFeatureCollection][nFeature][nLineString][3] + `", "Width": ` + prjGeom[nFeatureCollection][nFeature][nLineString][4] +
+          `,"Desc": "` + prjGeom[nFeatureCollection][nFeature][nLineString][5] + `"}`
 
         console.log(propertiesText);
 
       }
-      for (var d = 0; d < 10; d++) {
-        console.log("coordinates: " + d);
-        console.log(prjGeom[a][b][0][d]);
+      for (var nCoordinate = 0; nCoordinate < 10; nCoordinate++) {
+        console.log("coordinates: " + nCoordinate);
+        console.log(prjGeom[nFeatureCollection][nFeature][0][nCoordinate]);
 
-        if (d + 1 < 10) {
-          coordinateBracket = `[` + prjGeom[a][b][0][d] + `],`;
+        if (nCoordinate + 1 < 10) {
+          coordinateBracket = `[` + prjGeom[nFeatureCollection][nFeature][0][nCoordinate] + `],`;
         } else {
-          coordinateBracket = `[` + prjGeom[a][b][0][d] + `]`;
+          coordinateBracket = `[` + prjGeom[nFeatureCollection][nFeature][0][nCoordinate] + `]`;
         }
 
         coordinates += coordinateBracket;
@@ -47,7 +50,7 @@ function parseGeometryToGeoJSON(prjGeom) {
     }
 
 
-    featureText = `{ "type": "Feature", ` + propertiesText + `,` + geometryText + `}, "id": "` + a + `" }`;
+    featureText = `{ "type": "Feature", ` + propertiesText + `,` + geometryText + `}, "id": "` + nFeatureCollection + `" }`;
     console.log(featureText);
   }
   featureCollectionText = `{"type": "FeatureCollection", "metadata": {}, "features": [` + featureText + `]}`;
