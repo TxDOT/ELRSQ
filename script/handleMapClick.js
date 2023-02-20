2a85b0f3d3c485350d48e2e54b73181210ff534d
Eric Schall <eschal-c@txdot.gov>
2023-02-19 19:26:29 -0600

    HTML hygiene

--- script/handleMapClick.js
+++ script/handleMapClick.js
@@ -1,5 +1,5 @@
 function handleMapClick(evt) {
-  var oncursortab = document.querySelectorAll("#mapcursor-justified.active");
+  var oncursortab = document.querySelectorAll("#mapcursor-tabpane.active");
   if (oncursortab[0]) {
     coordinateQuery(evt.mapPoint.latitude, evt.mapPoint.longitude);
   }
