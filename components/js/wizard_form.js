4149ccf14fb83621f82a77d6127f40de65962791
Eric Schall <eschal-c@txdot.gov>
2023-02-20 17:44:39 -0600

    wizard cleanup

--- components/js/wizard_form.js
+++ components/js/wizard_form.js
@@ -1,12 +1,12 @@
-const staticWizard_Form = `                <!-- Horizontal Form -->
+const staticWizard_Form = `                
+<!-- Horizontal Form -->
 <form>
   <div class="row mb-3" id="rowInputControlSection">
     <label for="inputControlSection" class="col-sm-4 col-form-label">Control Section</label>
     <div class="input-group mb-3 col-sm-4">
       <input id="wizinputControlSection" type="text" name="controlsection" class="form-control textentry"
         placeholder="012201">
-      <button id="missingControlSection" class="btn btn-outline-danger" type="button"
-        onclick="noControlSection()">Unknown</button>
+      <button id="missingControlSection" class="btn btn-outline-danger" type="button">Unknown</button>
     </div>
   </div>
 
@@ -14,12 +14,10 @@ const staticWizard_Form = `                <!-- Horizontal Form -->
   <div class="row mb-3" id="rowInputMilepointMeasure">
     <label for="inputMilepointMeasure" class="col-sm-4 col-form-label">Milepoint Measure</label>
     <div class="input-group mb-3 col-sm-4">
-      <input id="wizinputMilepointMeasure" type="number" name="mpm" class="form-control textentry"
-        placeholder="2.394">
-      <button class="btn btn-outline-primary" type="button" onclick="lrsQuery(3, 0, 'inputControlSection', 'inputMilepointMeasure')"
-        title="Convert to other LRS"><i class="fa fa-cog" aria-hidden="true"></i> Convert</button>
-      <button id="missingMilepointMeasure" class="btn btn-outline-danger" type="button"
-        onclick="noMilepointMeasure()">Unknown</button>
+      <input id="wizinputMilepointMeasure" type="number" name="mpm" class="form-control textentry" placeholder="2.394">
+      <button id="convert3-wizard" class="btn btn-outline-primary" type="button" title="Convert to other LRS">
+      <i class="fa fa-cog" aria-hidden="true"></i> Convert</button>
+      <button id="missingMilepointMeasure" class="btn btn-outline-danger" type="button">Unknown</button>
     </div>
   </div>
 
@@ -29,8 +27,7 @@ const staticWizard_Form = `                <!-- Horizontal Form -->
     <div class="input-group mb-3 col-sm-4">
       <input id="wizinputRouteName" type="text" pattern="[A-Z]{2}\d{4}[A-Z]?-[A-Z]{2}" name="routename"
         placeholder="US0077-KG" class="form-control textentry">
-      <button id="missingRouteName" class="btn btn-outline-danger" type="button"
-        onclick="noRouteName()">Unknown</button>
+      <button id="missingRouteName" class="btn btn-outline-danger" type="button">Unknown</button>
     </div>
   </div>
 
@@ -39,10 +36,9 @@ const staticWizard_Form = `                <!-- Horizontal Form -->
     <div class="input-group mb-3 col-sm-4">
       <input id="wizinputDistanceFromOrigin" type="number" name="dfo" placeholder="1.606"
         class="form-control textentry">
-      <button class="btn btn-outline-primary" type="button" onclick="lrsQuery(4, 0, 'inputRouteName', 'inputDistanceFromOrigin')"
-        title="Convert to other LRS"><i class="fa fa-cog" aria-hidden="true"></i> Convert</button>
-      <button id="missingDistanceFromOrigin" class="btn btn-outline-danger" type="button"
-        onclick="noDistanceFromOrigin()">Unknown</button>
+      <button id="convert4-wizard" class="btn btn-outline-primary" type="button" title="Convert to other LRS">
+      <i class="fa fa-cog" aria-hidden="true"></i> Convert</button>
+      <button id="missingDistanceFromOrigin" class="btn btn-outline-danger" type="button">Unknown</button>
     </div>
   </div>
 
@@ -53,18 +49,18 @@ const staticWizard_Form = `                <!-- Horizontal Form -->
         class="form-control textentry">
       <input id="wizinputDisplacement" type="number" name="displacement" placeholder="0.065"
         class="form-control textentry">
-      <button class="btn btn-outline-primary" type="button" onclick="lrsQuery(2, 0, 'inputRouteName', 'inputReferenceMarker', 'inputDisplacement')"
-        title="Convert to other LRS"><i class="fa fa-cog" aria-hidden="true"></i> Convert</button>
-      <button id="missingReferenceMarker" class="btn btn-outline-danger" type="button"
-        onclick="noReferenceMarker()">Unknown</button>
+      <button id="convert2-wizard" class="btn btn-outline-primary" type="button" title="Convert to other LRS">
+        <i class="fa fa-cog" aria-hidden="true"></i> Convert</button>
+      <button id="missingReferenceMarker" class="btn btn-outline-danger" type="button">Unknown</button>
     </div>
   </div>
 
-
+  <!-- TODO Wizard: enable this -->
   <div class="row mb-3" style="display:none" id="rowInputMatchCoordinates">
     <label class="col-sm-4 col-form-label">Match Against:</label>
     <div class="form-check form-check-inline">
-      <input class="form-check-input" type="radio" name="inlineRadioOptions" id="matchcontrolsection" value="controlsection">
+      <input class="form-check-input" type="radio" name="inlineRadioOptions" id="matchcontrolsection"
+        value="controlsection">
       <label class="form-check-label" for="inlineRadio1">Control Section</label>
     </div>
     <div class="form-check form-check-inline">
@@ -77,31 +73,29 @@ const staticWizard_Form = `                <!-- Horizontal Form -->
   <div class="row mb-3" style="display:none" id="rowInputCoordinates">
     <label class="col-sm-4 col-form-label">Latitude & Longitude</label>
     <div class="input-group">
-      <input id="wizinputLatitude" type="number" name="lat" placeholder="29.39780972"
-        class="form-control textentry">
-      <input id="wizinputLongitude" type="number" name="lon" placeholder="-94.98759004"
-        class="form-control textentry">
-      <button class="btn btn-outline-primary" type="button" onclick="lrsQuery(1, 0, 'inputLatitude', 'inputLongitude')"
-        title="Convert to other LRS"><i class="fa fa-cog" aria-hidden="true"></i> Convert</button>
-      <button id="missingCoordinates" class="btn btn-outline-danger" type="button"
-        onclick="restartWizard()">Restart</button>
+      <input id="wizinputLatitude" type="number" name="lat" placeholder="29.39780972" class="form-control textentry">
+      <input id="wizinputLongitude" type="number" name="lon" placeholder="-94.98759004" class="form-control textentry">
+      <button id="convert1-wizard" class="btn btn-outline-primary" type="button" title="Convert to other LRS">
+        <i class="fa fa-cog" aria-hidden="true"></i> Convert</button>
+      <button id="missingCoordinates" class="btn btn-outline-danger" type="button">Restart</button>
     </div>
   </div>
 
-  <div class="btn-toolbar mb-3 justify-content-center" role="toolbar"
-    aria-label="Toolbar with button groups">
+  <div class="btn-toolbar mb-3 justify-content-center" role="toolbar" aria-label="Toolbar with button groups">
     <button type="reset" class="btn btn-secondary" title="Reset to default values"><i class="fa fa-undo"
         aria-hidden="true"></i> Reset</button>
   </div>
-</form><!-- End Horizontal Form -->`
+</form>
+<!-- End Horizontal Form -->
+`
 
 
 class Wizard_Form extends HTMLElement {
 
-  constructor () {
+  constructor() {
     super();
   }
-  
+
   connectedCallback() {
     this.innerHTML = staticWizard_Form;
   }
