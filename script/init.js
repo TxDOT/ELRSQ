cdc7bb7697cde5e0cc498ffa9f5c7accdabeaed8
Eric Schall <eschal-c@txdot.gov>
2023-02-19 19:42:31 -0600

    moved global variables into init.js

---
+++ script/init.js
@@ -0,0 +1,45 @@
+let currentSelection = [];
+let allResults = [];
+let currentPos = 1;
+let graphics = [];
+
+let projects = [
+    [
+        "IH0035-KG",
+        "121.243",
+        "149.576",
+        "#ff8000",
+        4,
+        "35"
+    ],
+    [
+        "IH0045-KG",
+        "1.243",
+        "9.576",
+        "#ff8000",
+        4,
+        "45"
+    ],
+    [
+        "IH0010-KG",
+        "500",
+        "600",
+        "#ff8000",
+        4,
+        "10"
+    ]
+]
+
+//let projects = [];
+var projectLines = [];
+
+
+
+if (screen.width >= 768) {
+    const GUTTER_SIZE = 30;
+    Split(['#split-0', '#split-1'], {
+        sizes: [33, 66],
+        minSize: [300, 400],
+        expandToMin: false
+    });
+}
