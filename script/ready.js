d605839025d927921abef4c3cebc06f4d217dbaa
Eric Schall <eschal-c@txdot.gov>
2023-02-19 19:55:56 -0600

    taking out trash

--- script/ready.js
+++
@@ -1,7 +0,0 @@
-function GreenToYellow(){
-    document.getElementById('readyIndicator').classList.replace('green', 'yellow');
-}
-
-function YellowToGreen(){
-    document.getElementById('readyIndicator').classList.replace('yellow', 'green');
-}
