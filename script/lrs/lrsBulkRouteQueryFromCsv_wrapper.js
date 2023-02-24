7d9960726b23e4ecb87a2937f5ca54e5e090b430
Eric Schall <eschal-c@txdot.gov>
2023-02-24 16:30:46 -0600

    renaming and refactoring LRS functions

--- script/lrs/lrsBulkRouteQueryFromCsv_wrapper.js
+++ script/lrs/lrsBulkRouteQueryFromCsv_wrapper.js
@@ -15,7 +15,97 @@ async function lrsBulkRouteQueryFromCsv_wrapper(fileContents) {
   let lrm_indices = field_indices[0];
   let other_indices = field_indices[1];
 
-  await lrsBulkRouteQueryFromCsv(currentLRMno, parsedInputCSV, lrm_indices, other_indices);
+  //set begin indices
+  let b_lrm_indices = [];
+  let e_lrm_indices = [];
+  //set begin indices
+
+  let rte_nm_lrm_indices = '';
+
+  GreenToYellow();
+
+  // make array for output
+  let refinedData = [];
+
+  // set title keys
+  let titleKeys = other_indices.map(i => parsedInputCSV[0][i]).concat(lrsApiFields.map(i => 'BEGIN_' + i)).concat(lrsApiFields.map(i => 'END_' + i));
+
+
+  // get indices
+  if (currentLRMno == 1) {
+    b_lrm_indices = [lrm_indices[0], lrm_indices[1]];
+    e_lrm_indices = [lrm_indices[2], lrm_indices[3]];
+    rte_nm_lrm_indices = lrm_indices[4];
+  }
+
+  else if (currentLRMno == 2) {
+    b_lrm_indices = [lrm_indices[0], lrm_indices[1], lrm_indices[2]];
+    e_lrm_indices = [lrm_indices[0], lrm_indices[3], lrm_indices[4]];
+    rte_nm_lrm_indices = lrm_indices[0];
+  }
+
+  else if (currentLRMno == 3) {
+    b_lrm_indices = [lrm_indices[0], lrm_indices[1]];
+    e_lrm_indices = [lrm_indices[2], lrm_indices[3]];
+    rte_nm_lrm_indices = lrm_indices[4];
+  }
+
+  else if (currentLRMno == 4) {
+    b_lrm_indices = [lrm_indices[0], lrm_indices[1]];
+    e_lrm_indices = [lrm_indices[0], lrm_indices[2]];
+    rte_nm_lrm_indices = lrm_indices[0];
+  }
+  // end get indices
+
+  // process rows
+  for (let rowToQuery = 1; rowToQuery < parsedInputCSV.length; rowToQuery++) {
+    // skipping 0 header row
+    console.log("row " + (rowToQuery + 1) + " of " + parsedInputCSV.length);
+    let routeQueryOutput = [];
+
+    let user_input_rte_nm = fixThisVerySpecificTextFormat(parsedInputCSV[rowToQuery][rte_nm_lrm_indices]);
+
+    // build url
+    let B_url = makeLrsQueryUrlFromIndex(currentLRMno, parsedInputCSV[rowToQuery], b_lrm_indices, 1); // FIXME have this take function as argument
+    let E_url = makeLrsQueryUrlFromIndex(currentLRMno, parsedInputCSV[rowToQuery], e_lrm_indices, 1);
+
+    console.log(B_url);
+    console.log(E_url);
+    // end build url
+
+    // perform query
+    const B_results = await queryService(B_url);
+    const E_results = await queryService(E_url);
+    // end perform query
+
+    //get right route
+    let routeResulrsArr = rteOutputAssembler(routeQueryOutput, currentLRMno, B_results, E_results, user_input_rte_nm);
+    // end get right route
+
+    // get row header data
+    let rowhead = other_indices.map(i => parsedInputCSV[rowToQuery][i]);
+
+    // assemble data
+    let fullRowData = rowhead.concat(routeResulrsArr);
+    refinedData.push(fullRowData);
+
+
+
+
+
+  }
+
+  // append feature info
+  refinedData.unshift(titleKeys);
+
+
+  // show results
+  // future feature showBulkRouteResults(refinedData);
+
+  // export data
+  //TODO tabular export
+  tabularRoutesConvertExport(refinedData);
+
+  YellowToGreen();
+
 }
-
-
