652189ee46ab550aa55f605181b9e00faf3fc47d
Eric Schall <eschal-c@txdot.gov>
2023-02-24 16:25:14 -0600

    renaming and refactoring LRS functions

--- script/lrs/lrsBulkRouteQueryFromCsv.js
+++ script/lrs/lrsBulkRouteQueryFromCsv.js
@@ -2,8 +2,11 @@ async function lrsBulkRouteQueryFromCsv(method, parsedInputCSV, lrm_indices, oth
   // bulk 
   // route
 
+  //set begin indices
   let b_lrm_indices = [];
   let e_lrm_indices = [];
+  //set begin indices
+
   let rte_nm_lrm_indices = '';
 
   GreenToYellow();
@@ -12,35 +15,34 @@ async function lrsBulkRouteQueryFromCsv(method, parsedInputCSV, lrm_indices, oth
   let refinedData = [];
 
   // set title keys
-  let titleKeys = other_indices.map(i => parsedInputCSV[0][i]).concat(lrsApiFields.map(i => 'BEGIN_' + i)).concat(lrsApiFields.map(i => 'END_' + i)); //FIXME use all caps and underscore
+  let titleKeys = other_indices.map(i => parsedInputCSV[0][i]).concat(lrsApiFields.map(i => 'BEGIN_' + i)).concat(lrsApiFields.map(i => 'END_' + i));
 
 
   // get indices
-  if (method == 1) {
+  if (currentLRMno == 1) {
     b_lrm_indices = [lrm_indices[0], lrm_indices[1]];
-    e_lrm_indices = [lrm_indices[3], lrm_indices[3]];
+    e_lrm_indices = [lrm_indices[2], lrm_indices[3]];
     rte_nm_lrm_indices = lrm_indices[4];
   }
 
-  else if (method == 2) {
+  else if (currentLRMno == 2) {
     b_lrm_indices = [lrm_indices[0], lrm_indices[1], lrm_indices[2]];
     e_lrm_indices = [lrm_indices[0], lrm_indices[3], lrm_indices[4]];
     rte_nm_lrm_indices = lrm_indices[0];
   }
 
-  else if (method == 3) {
+  else if (currentLRMno == 3) {
     b_lrm_indices = [lrm_indices[0], lrm_indices[1]];
-    e_lrm_indices = [lrm_indices[3], lrm_indices[3]];
+    e_lrm_indices = [lrm_indices[2], lrm_indices[3]];
     rte_nm_lrm_indices = lrm_indices[4];
   }
 
-  else if (method == 4) {
+  else if (currentLRMno == 4) {
     b_lrm_indices = [lrm_indices[0], lrm_indices[1]];
     e_lrm_indices = [lrm_indices[0], lrm_indices[2]];
     rte_nm_lrm_indices = lrm_indices[0];
   }
   // end get indices
-
 
   // process rows
   for (let rowToQuery = 1; rowToQuery < parsedInputCSV.length; rowToQuery++) {
@@ -51,12 +53,11 @@ async function lrsBulkRouteQueryFromCsv(method, parsedInputCSV, lrm_indices, oth
     let user_input_rte_nm = fixThisVerySpecificTextFormat(parsedInputCSV[rowToQuery][rte_nm_lrm_indices]);
 
     // build url
-    let B_url = makeLrsQueryUrlFromIndex(method, parsedInputCSV[rowToQuery], b_lrm_indices, 1); // FIXME have this take function as argument
-    let E_url = makeLrsQueryUrlFromIndex(method, parsedInputCSV[rowToQuery], e_lrm_indices, 1);
+    let B_url = makeLrsQueryUrlFromIndex(currentLRMno, parsedInputCSV[rowToQuery], b_lrm_indices, 1); // FIXME have this take function as argument
+    let E_url = makeLrsQueryUrlFromIndex(currentLRMno, parsedInputCSV[rowToQuery], e_lrm_indices, 1);
 
     console.log(B_url);
     console.log(E_url);
-
     // end build url
 
     // perform query
@@ -64,16 +65,29 @@ async function lrsBulkRouteQueryFromCsv(method, parsedInputCSV, lrm_indices, oth
     const E_results = await queryService(E_url);
     // end perform query
 
+    //get right route
     let routeResulrsArr = rteOutputAssembler(routeQueryOutput, method, B_results, E_results, user_input_rte_nm);
+    // end get right route
+
+    // get row header data
+    let rowhead = other_indices.map(i => parsedInputCSV[rowToQuery][i]);
 
     // assemble data
-    let rowhead = other_indices.map(i => parsedInputCSV[rowToQuery][i]);
     let fullRowData = rowhead.concat(routeResulrsArr);
     refinedData.push(fullRowData);
+
+
+
+
+
   }
 
   // append feature info
   refinedData.unshift(titleKeys);
+
+
+  // show results
+  // future feature showBulkRouteResults(refinedData);
 
   // export data
   //TODO tabular export
--- script/lrs/lrsBulkRouteQueryFromCsv_wrapper.js
+++ script/lrs/lrsBulkRouteQueryFromCsv_wrapper.js
@@ -1,20 +1,21 @@
-
 async function lrsBulkRouteQueryFromCsv_wrapper(fileContents) {
   // bulk 
   // route
 
-
   // input CSV
   let parsedInputCSV = Papa.parse(fileContents, { "skipEmptyLines": true }).data;
 
-  let method = 2; //FIXME set this programatically
+  currentLRMno = 2; //FIXME set this programatically
+  //lrm_indices = [0, 1, 2];
+  currentPointFieldOrder = ['inputRouteName_2', 'inputReferenceMarker', 'inputDisplacement'];
+  currentRouteFieldOrder = ['inputRouteName_2', 'inputBeginReferenceMarker', 'inputBeginDisplacement', 'inputEndReferenceMarker', 'inputEndDisplacement'];
 
   // set fields
-  let field_indices = await setTableFieldsByMethod(method, parsedInputCSV);
+  let field_indices = await setTableFieldsByMethod(currentLRMno, parsedInputCSV);
   let lrm_indices = field_indices[0];
   let other_indices = field_indices[1];
 
-  await lrsBulkRouteQueryFromCsv(method, parsedInputCSV, lrm_indices, other_indices);
+  await lrsBulkRouteQueryFromCsv(currentLRMno, parsedInputCSV, lrm_indices, other_indices);
 }
 
 
