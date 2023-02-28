b8debce2214d80d71867e82718180adf32f7d21e
Eric Schall <eschal-c@txdot.gov>
2023-02-27 20:45:18 -0600

    lrs initiators

--- script/lrs_initiators.js
+++ script/lrs_initiators.js
@@ -1,11 +1,15 @@
+
+let inputMethod = '';
+
+
 //FIXME this is not always true
 let currentLRM = `coordinates-tab`;
 let currentLRMno = 1;
-let lrm_indices = [0, 1];
-let routlrm_indices1 = [0, 1, 2, 3];
-default_templatlrm_indices1 = [2, 1];
-let currentPointFieldOrder = ['inputLatitude', 'inputLongitude'];
-let currentRouteFieldOrder = ['inputBeginLatitude', 'inputBeginLongitude', 'inputEndLatitude', 'inputEndLongitude'];
+// let lrm_indices = [0, 1];
+// let routlrm_indices1 = [0, 1, 2, 3];
+// default_templatlrm_indices1 = [2, 1];
+// let currentPointFieldOrder = ['inputLatitude', 'inputLongitude'];
+// let currentRouteFieldOrder = ['inputBeginLatitude', 'inputBeginLongitude', 'inputEndLatitude', 'inputEndLongitude'];
 
 // get current LRM
 
@@ -43,44 +47,47 @@ $('button[data-bs-toggle="tab"]').on('shown.bs.tab', function (e) {
 
   if (currentLRM == `referencemarker-tab`) {
     currentLRMno = 2;
-    lrm_indices = [0, 1, 2];
-    routlrm_indices1 = [0, 1, 2, 3, 4];
-    default_templatlrm_indices1 = [1, 2, 3];
-    currentPointFieldOrder = ['inputRouteName_2', 'inputReferenceMarker', 'inputDisplacement'];
-    currentRouteFieldOrder = ['inputRouteName_2', 'inputBeginReferenceMarker', 'inputBeginDisplacement', 'inputEndReferenceMarker', 'inputEndDisplacement'];
+    // lrm_indices = [0, 1, 2];
+    // routlrm_indices1 = [0, 1, 2, 3, 4];
+    // default_templatlrm_indices1 = [1, 2, 3];
+    // currentPointFieldOrder = ['inputRouteName_2', 'inputReferenceMarker', 'inputDisplacement'];
+    // currentRouteFieldOrder = ['inputRouteName_2', 'inputBeginReferenceMarker', 'inputBeginDisplacement', 'inputEndReferenceMarker', 'inputEndDisplacement'];
   } else if (currentLRM == `controlsection-tab`) {
     currentLRMno = 3;
-    lrm_indices = [0, 1];
-    routlrm_indices1 = [0, 1, 2, 3];
-    default_templatlrm_indices1 = [1, 2];
-    currentPointFieldOrder = ['inputControlSection', 'inputMilepointMeasure'];
-    currentRouteFieldOrder = ['inputBeginControlSection', 'inputBeginMilepointMeasure', 'inputEndControlSection', 'inputEndMilepointMeasure'];
+    // lrm_indices = [0, 1];
+    // routlrm_indices1 = [0, 1, 2, 3];
+    // default_templatlrm_indices1 = [1, 2];
+    // currentPointFieldOrder = ['inputControlSection', 'inputMilepointMeasure'];
+    // currentRouteFieldOrder = ['inputBeginControlSection', 'inputBeginMilepointMeasure', 'inputEndControlSection', 'inputEndMilepointMeasure'];
   } else if (currentLRM == `distancefromorigin-tab`) {
     currentLRMno = 4;
-    lrm_indices = [0, 1];
-    routlrm_indices1 = [0, 1, 2];
-    default_templatlrm_indices1 = [1, 2];
-    currentPointFieldOrder = ['inputRouteName_4', 'inputDistanceFromOrigin'];
-    currentRouteFieldOrder = ['inputRouteName_4', 'inputBeginDistanceFromOrigin', 'inputEndDistanceFromOrigin'];
+    // lrm_indices = [0, 1];
+    // routlrm_indices1 = [0, 1, 2];
+    // default_templatlrm_indices1 = [1, 2];
+    // currentPointFieldOrder = ['inputRouteName_4', 'inputDistanceFromOrigin'];
+    // currentRouteFieldOrder = ['inputRouteName_4', 'inputBeginDistanceFromOrigin', 'inputEndDistanceFromOrigin'];
   } else {
     currentLRMno = 1;
-    lrm_indices = [0, 1];
-    routlrm_indices1 = [0, 1, 2, 3];
-    default_templatlrm_indices1 = [2, 1];
-    currentPointFieldOrder = ['inputLatitude', 'inputLongitude'];
-    currentRouteFieldOrder = ['inputBeginLatitude', 'inputBeginLongitude', 'inputEndLatitude', 'inputEndLongitude'];
+    // lrm_indices = [0, 1];
+    // routlrm_indices1 = [0, 1, 2, 3];
+    // default_templatlrm_indices1 = [2, 1];
+    // currentPointFieldOrder = ['inputLatitude', 'inputLongitude'];
+    // currentRouteFieldOrder = ['inputBeginLatitude', 'inputBeginLongitude', 'inputEndLatitude', 'inputEndLongitude'];
   }
 
 });
 
 // end get current LRM
 
-
+// begin event handler
 
 if (calcGeomType == "Point") {
   // 1-point
 
-  $(".convert-1point").on('click', function () { lrsSinglePointQuery(currentLRMno, lrm_indices, currentPointFieldOrder); });
+  $(".convert-1point").on('click', function () {
+    inputMethod = "html";
+    lrsSinglePointQuery(currentLRMno, inputMethod);
+  });
 
 
   // 1-point
@@ -113,7 +120,8 @@ if (calcGeomType == "Point") {
       GreenToYellow();
       const fileContents = await readFile(e.target.files[0])
       YellowToGreen();
-      lrsBulkPointQuery(currentLRMno, default_templatlrm_indices1, fileContents);
+      inputMethod = "table";
+      lrsBulkPointQuery(currentLRMno, fileContents);
     });
 
 
@@ -127,32 +135,38 @@ if (calcGeomType == "Point") {
   // single
   // wizard
 
-  $(document).ready(function () {
-
-    $("wizard_form-component").load("components/html/wizard_form.html", function (response, status, xhr) {
-      if (status == "error") { $("wizard_form-component").html(staticWizard_Form); }
-      $("#missingControlSection").on('click', function () { noControlSection(); });
-      $("#convert3-wizard").on('click', function () { lrsSinglePointQuery(3, [0, 1], ['inputControlSection', 'inputMilepointMeasure']); });
-      $("#missingMilepointMeasure").on('click', function () { noMilepointMeasure(); });
-      $("#missingRouteName").on('click', function () { noRouteName(); });
-      $("#convert4-wizard").on('click', function () { lrsSinglePointQuery(4, [0, 1], ['inputRouteName_4', 'inputDistanceFromOrigin']); });
-      $("#missingDistanceFromOrigin").on('click', function () { noDistanceFromOrigin(); });
-      $("#convert2-wizard").on('click', function () { lrsSinglePointQuery(2, [0, 1, 2], ['inputRouteName_2', 'inputReferenceMarker', 'inputDisplacement']); });
-      $("#missingReferenceMarker").on('click', function () { noReferenceMarker(); });
-      $("#convert1-wizard").on('click', function () { lrsSinglePointQuery(1, [0, 1], ['inputLatitude', 'inputLongitude']); });
-      $("#missingCoordinates").on('click', function () { restartWizard(); });
+  /**
+    $(document).ready(function () {
+  
+      $("wizard_form-component").load("components/html/wizard_form.html", function (response, status, xhr) {
+        if (status == "error") { $("wizard_form-component").html(staticWizard_Form); }
+        $("#missingControlSection").on('click', function () { noControlSection(); });
+        $("#convert3-wizard").on('click', function () { lrsSinglePointQuery(3, [0, 1], ['inputControlSection', 'inputMilepointMeasure']); });
+        $("#missingMilepointMeasure").on('click', function () { noMilepointMeasure(); });
+        $("#missingRouteName").on('click', function () { noRouteName(); });
+        $("#convert4-wizard").on('click', function () { lrsSinglePointQuery(4, [0, 1], ['inputRouteName_4', 'inputDistanceFromOrigin']); });
+        $("#missingDistanceFromOrigin").on('click', function () { noDistanceFromOrigin(); });
+        $("#convert2-wizard").on('click', function () { lrsSinglePointQuery(2, [0, 1, 2], ['inputRouteName_2', 'inputReferenceMarker', 'inputDisplacement']); });
+        $("#missingReferenceMarker").on('click', function () { noReferenceMarker(); });
+        $("#convert1-wizard").on('click', function () { lrsSinglePointQuery(1, [0, 1], ['inputLatitude', 'inputLongitude']); });
+        $("#missingCoordinates").on('click', function () { restartWizard(); });
+      });
+  
     });
-
-  });
+  */
 
 
 }
 
+// begin event handler
 if (calcGeomType == "Route") {
   // 2-point
   // single
 
-  $(".convert-2point").on('click', function () { lrsSingleRouteQuery(currentLRMno, routlrm_indices1, currentRouteFieldOrder); });
+  $(".convert-2point").on('click', function () {
+    inputMethod = "html";
+    lrsSingleRouteQuery(currentLRMno, inputMethod);
+  });
 
 
   // 2-point
@@ -184,11 +198,9 @@ if (calcGeomType == "Route") {
       GreenToYellow();
       const fileContents = await readFile(e.target.files[0])
       YellowToGreen();
-      lrsBulkRouteQuery(currentLRMno, routlrm_indices1, fileContents);
+      inputMethod = "table";
+      lrsBulkRouteQuery(currentLRMno, fileContents);
     });
-
-
-
 
 
   });
