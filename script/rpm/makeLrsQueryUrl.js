2da73008a3b6fffaae911ddd6dc761ea1bb08416
Eric Schall <eschal-c@txdot.gov>
2023-02-28 21:50:39 -0600

    file organization

---
+++ script/rpm/makeLrsQueryUrl.js
@@ -0,0 +1,66 @@
+function makeLrsQueryUrl(inputMethod, lrsMethod, lrm_indices, rowFromArray, format_rte_nm) {
+
+  // build url
+  let url = '';
+
+  let routeName = '';
+
+  let lat = '';
+  let lon = '';
+  let refMarker = '';
+  let displacement = '';
+  let controlSecNum = '';
+  let milePointMeasure = '';
+  let dfo = '';
+
+  let coordinateArr = [];
+
+  if (inputMethod == 'html') {
+    for (let i = 0; i < lrm_indices.length; i++) {
+      let value = $('#' + rowFromArray[lrm_indices[i]]).val();
+      console.log('#' + rowFromArray[lrm_indices[i]]);
+      console.log(value);
+      coordinateArr.push(value);
+    }
+  } else if (inputMethod == 'table') {
+    coordinateArr = rowFromArray;
+    //FIXME map lrm_indices somehow
+  }
+
+  if (format_rte_nm == 1) {
+    routeName = fixThisVerySpecificTextFormat(coordinateArr[lrm_indices[0]]);
+  } else {
+    routeName = coordinateArr[lrm_indices[0]];
+  }
+
+  if (lrsMethod == 1) {
+    lat = coordinateArr[0];
+    lon = coordinateArr[1];
+    url = `https://lrs-ext.us-e1.cloudhub.io/api/elrs1?Lat=${lat}&Lon=${lon}`;
+  }
+
+  else if (lrsMethod == 2) {
+    refMarker = coordinateArr[1];
+    displacement = coordinateArr[2];
+    url = `https://lrs-ext.us-e1.cloudhub.io/api/elrs2?RouteID=${routeName}&ReferenceMarker=${refMarker}&Displacement=${displacement}`;
+  }
+
+  else if (lrsMethod == 3) {
+    controlSecNum = coordinateArr[0];
+    milePointMeasure = coordinateArr[1];
+    url = `https://lrs-ext.us-e1.cloudhub.io/api/elrs3?ControlSectionNumber=${controlSecNum}&MilePointMeasure=${milePointMeasure}`;
+  }
+
+  else if (lrsMethod == 4) {
+    dfo = coordinateArr[1];
+    url = `https://lrs-ext.us-e1.cloudhub.io/api/elrs4?RouteID=${routeName}&DistanceFromOrigin=${dfo}`;
+  }
+
+  console.log(url);
+  return url;
+
+  // end build url
+}
+
+
+
---
+++ script/rpm/rpm_initiators.js
@@ -0,0 +1,51 @@
+let currentLRM = `coordinates-tab`;
+let currentLRMno = 1;
+let route_lrm_indices = [0, 1, 2, 3];
+default_template_lrm_indices = [2, 1];
+let currentRouteFieldOrder = ['inputBeginLatitude', 'inputBeginLongitude', 'inputEndLatitude', 'inputEndLongitude'];
+
+// get current LRM
+
+$('button[data-bs-toggle="tab"]').on('shown.bs.tab', function (e) {
+  currentLRM = $(e.target).attr("id") // activated tab
+  console.log(currentLRM);
+
+
+  if (currentLRM == `mapcursor-tab`) {
+    $("#viewDiv").css('cursor', 'crosshair');
+  } else {
+    $("#viewDiv").css('cursor', 'move');
+  }
+
+  if (currentLRM == `referencemarker-tab`) {
+    currentLRMno = 2;
+    route_lrm_indices = [0, 1, 2, 3, 4];
+    currentRouteFieldOrder = ['inputRouteName_2', 'inputBeginReferenceMarker', 'inputBeginDisplacement', 'inputEndReferenceMarker', 'inputEndDisplacement'];
+  } else if (currentLRM == `controlsection-tab`) {
+    currentLRMno = 3;
+    route_lrm_indices = [0, 1, 2, 3];
+    currentRouteFieldOrder = ['inputBeginControlSection', 'inputBeginMilepointMeasure', 'inputEndControlSection', 'inputEndMilepointMeasure'];
+  } else if (currentLRM == `distancefromorigin-tab`) {
+    currentLRMno = 4;
+    route_lrm_indices = [0, 1, 2];
+    currentRouteFieldOrder = ['inputRouteName_4', 'inputBeginDistanceFromOrigin', 'inputEndDistanceFromOrigin'];
+  } else {
+    currentLRMno = 1;
+    route_lrm_indices = [0, 1, 2, 3];
+    currentRouteFieldOrder = ['inputBeginLatitude', 'inputBeginLongitude', 'inputEndLatitude', 'inputEndLongitude'];
+  }
+
+});
+
+// end get current LRM
+
+
+
+if (calcGeomType == "Route") {
+  // 2-point
+  // single
+
+  $(".convert-2point").on('click', function () { lrsSingleRouteQuery_RPM(currentLRMno, route_lrm_indices, currentRouteFieldOrder); });
+}
+
+
