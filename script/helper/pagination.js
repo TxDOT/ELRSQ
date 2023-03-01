ba41b3aec1071c45281312159d77f5a29dcde06f
Eric Schall <eschal-c@txdot.gov>
2023-03-01 10:33:08 -0600

    pagination still working

--- script/helper/pagination.js
+++ script/helper/pagination.js
@@ -8,28 +8,37 @@ function resetCurrentPagination() {
   currentPagination = 1;
 }
 
+
+//navResults called by pagination buttons in showResults function
+// TODO move point on map
+function navResults(direction, results) {
+  direction == 'next' ? currentPagination++ : currentPagination--;
+
+  if (currentPagination > 0 && currentPagination <= results.length) {
+    showPointResults(results, currentPagination)
+  }
+}
+
+
 // determine pagination and fill in HTML table results
 //TODO move point on map
 function showPointResults(results, navIndex) {
   console.log("showPointResults");
-
-  allResults = results; // FIXME this is changing the value of a global variable
-  resultCount = allResults.length; // use this somewhere
   const index = navIndex ? navIndex - 1 : 0;
-  currentResult = allResults[index];
+  console.log(index);
 
-  //insert pagination
-  insertPagination(currentPagination, resultCount);
-  //paginationUpdater("#results-header", currentPagination, resultCount);
+  insertPagination(currentPagination, results);
+  //paginationUpdater("#results-header", currentPagination, results.length);
 
-  fillInHtmlTable(currentResult);
+  fillInHtmlTable(results[index]);
 }
 
 
 
 // FIXME just use Jquery to change pagination states
 //insert pagination
-function insertPagination(currentPagination, resultCount) {
+function insertPagination(currentPagination, results) {
+  resultCount = results.length;
   console.log(currentPagination + " of " + resultCount);
   const btn_prev_inactive = `<li class="page-item disabled"><span class="page-link" tabindex="-1" aria-disabled="true">Previous</span></li>`;
   const btn_next_inactive = `<li class="page-item disabled"><span class="page-link" tabindex="-1" aria-disabled="true">Next</span></li>`;
@@ -63,23 +72,14 @@ function insertPagination(currentPagination, resultCount) {
 
   //insert pagination
   $("#result-pagination").html(navTitle); // FIXME this is changing the inner HTML instead of dynamically creating elements
-  $("#pagn_prev").on('click', function () { navResults('prev'); }); //TODO move point on map
-  $("#pagn_next").on('click', function () { navResults('next'); }); // TODO separate out from specific function
+  $("#pagn_prev").on('click', function () { navResults('prev', results); }); //TODO move point on map
+  $("#pagn_next").on('click', function () { navResults('next', results); }); // TODO separate out from specific function
 
 }
 
 
 
-//navResults called by pagination buttons in showResults function
-// TODO move point on map
-// TODO separate out from specific function
-function navResults(direction) {
-  direction == 'next' ? currentPagination++ : currentPagination--;
 
-  if (currentPagination > 0 && currentPagination <= resultCount) {
-    showPointResults(allResults, currentPagination)
-  }
-}
 
 
 
@@ -174,13 +174,3 @@ function _paginationUpdater(someId, currentPagination, resultCount) {
 
 
 
-//navResults called by pagination buttons in showResults function
-// TODO move point on map
-// TODO separate out from specific function
-function _navResults(direction) {
-  direction == 'next' ? currentPagination++ : currentPagination--;
-
-  if (currentPagination > 0 && currentPagination <= resultCount) {
-    showPointResults(allResults, currentPagination)
-  }
-}
