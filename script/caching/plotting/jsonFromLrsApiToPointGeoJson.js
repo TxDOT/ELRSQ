288892f9059828ac3da057fd8a48ff78a8e78edd
Eric Schall <eschal-c@txdot.gov>
2023-03-14 20:29:55 -0500

    geojson conversion functions

--- script/caching/plotting/jsonFromLrsApiToPointGeoJson.js
+++ script/caching/plotting/jsonFromLrsApiToPointGeoJson.js
@@ -29,3 +29,86 @@ function jsonFromLrsApiToPointGeoJson(resultsArr) {
 
   return geojson;
 }
+
+
+function jsonFromLrsApiToPointGeoJson_singular(resultsArr) {
+
+  var geojson = {
+    type: "FeatureCollection",
+    features: [],
+  };
+
+  geojson.features.push({
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [resultsArr.LON, resultsArr.LAT]
+    },
+    "properties": {
+      "ROUTEID": resultsArr.ROUTEID,
+      "RTE_DEFN_LN_NM": resultsArr.RTE_DEFN_LN_NM,
+      "RDBD_TYPE_DSCR": resultsArr.RDBD_TYPE_DSCR,
+      "RTE_DFO": resultsArr.RTE_DFO,
+      "CTRL_SECT_LN_NBR": resultsArr.CTRL_SECT_LN_NBR,
+      "CTRL_SECT_MPT": resultsArr.CTRL_SECT_MPT,
+      "RMRKR_PNT_NBR": resultsArr.RMRKR_PNT_NBR,
+      "RMRKR_DISPLACEMENT": resultsArr.RMRKR_DISPLACEMENT
+    }
+  });
+
+  return geojson;
+}
+
+
+function jsonFromLrsApiToPointGeoJson_B(resultsArr) {
+
+  let aFeatureCollectionArray = [];
+
+  for (i = 0; i < resultsArr.length; i++) {
+    let aGeometryObj = new Object();
+    aGeometryObj.type = "Point";
+    aGeometryObj.coordinates = [resultsArr[i].LON, resultsArr[i].LAT];
+  
+    let aFeatureObj = new Object();
+    aFeatureObj.type = "Feature";
+    aFeatureObj.properties = resultsArr;
+    aFeatureObj.geometry = aGeometryObj;
+    aFeatureObj.id = "abc123";
+
+    aFeatureCollectionArray.push(aFeatureObj);
+  }
+
+  let aFeatureCollectionObj = new Object();
+  aFeatureCollectionObj.type = "FeatureCollection";
+  aFeatureCollectionObj.metadata = [];
+  aFeatureCollectionObj.features = aFeatureCollectionArray;
+
+  return aFeatureCollectionObj;
+
+}
+
+
+function jsonFromLrsApiToPointGeoJson_singular_B(myPrjAttributes) {
+
+  let aGeometryObj = new Object();
+  aGeometryObj.type = "Point";
+  aGeometryObj.coordinates = [myPrjAttributes.LON, myPrjAttributes.LAT];
+
+  let aFeatureObj = new Object();
+  aFeatureObj.type = "Feature";
+  aFeatureObj.properties = myPrjAttributes;
+  aFeatureObj.geometry = aGeometryObj;
+  aFeatureObj.id = "abc123";
+
+  let aFeatureCollectionArray = [(aFeatureObj)];
+
+  let metadata = [];
+  let aFeatureCollectionObj = new Object();
+  aFeatureCollectionObj.type = "FeatureCollection";
+  aFeatureCollectionObj.metadata = metadata;
+  aFeatureCollectionObj.features = aFeatureCollectionArray;
+
+  return aFeatureCollectionObj;
+}
+
+
--- script/caching/plotting/objectifyRouteProject.js
+++ script/caching/plotting/objectifyRouteProject.js
@@ -18,3 +18,26 @@ function objectifyRouteProject(currentResult) {
   return projObj;
 
 }
+
+
+
+function objectifyPointProject(currentResult) {
+  console.log("objectifyPointProject: currentResult: ");
+  console.log(currentResult);
+  let projObj = new Object();
+  projObj.RTE_NM = currentResult['RTE_DEFN_LN_NM'];
+  projObj.LAT = currentResult['LAT'];
+  projObj.LON = currentResult['LON'];
+  projObj.Color = currentResult['Color'];
+  projObj.Width = currentResult['Width'];
+  projObj.Desc = currentResult['Feature'];
+
+  if (GLOBALSETTINGS.PrintProjGeom == 1) {
+    let projString = JSON.stringify(projObj);
+    console.log("objectifyPointProject: ");
+    console.log(projString);
+  }
+
+  return projObj;
+
+}
